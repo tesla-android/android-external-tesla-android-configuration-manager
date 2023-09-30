e0124630289684581b68173aa69f3bf6fa9de377
Michał Gapiński <mike@gapinski.eu>
2023-09-30 18:27:58 +0200

    Revert "Add property for VRR"
    This reverts commit bf81f0f8119db56953b2151ebb18a2bd375915a7.

--- tesla-android-configuration-manager.cpp
+++ tesla-android-configuration-manager.cpp
@@ -30,7 +30,6 @@ const char *VIRTUAL_DISPLAY_LOWRES_SYSTEM_PROPERTY_KEY = "persist.tesla-android.
 const char *VIRTUAL_DISPLAY_RENDERER_SYSTEM_PROPERTY_KEY = "persist.tesla-android.virtual-display.renderer";
 const char *VIRTUAL_DISPLAY_IS_RESPONSIVE_SYSTEM_PROPERTY_KEY = "persist.tesla-android.virtual-display.is_responsive";
 const char *VIRTUAL_DISPLAY_IS_H264_SYSTEM_PROPERTY_KEY = "persist.tesla-android.virtual-display.is_h264";
-const char *VIRTUAL_DISPLAY_IS_VARIABLE_REFRESH_SYSTEM_PROPERTY_KEY = "persist.tesla-android.virtual-display.is_variable_refresh";
 const char *HEADLESS_CONFIG_IS_ENABLED_PROPERTY_KEY = "persist.drm_hwc.headless.is_enabled";
 const char *HEADLESS_CONFIG_OVERRIDE_PROPERTY_KEY = "persist.drm_hwc.headless.config";
 const char *HEADLESS_CONFIG_LATCH_PROPERTY_KEY = "persist.drm_hwc.latch";
@@ -542,7 +541,6 @@ int main() {
     add_number_property(json, "isResponsive", get_system_property_int(VIRTUAL_DISPLAY_IS_RESPONSIVE_SYSTEM_PROPERTY_KEY), res);
     add_number_property(json, "isH264", get_system_property_int(VIRTUAL_DISPLAY_IS_H264_SYSTEM_PROPERTY_KEY), res);
     add_number_property(json, "isHeadless", get_system_property_int(HEADLESS_CONFIG_IS_ENABLED_PROPERTY_KEY), res);
-    add_number_property(json, "isVariableRefresh", get_system_property_int(VIRTUAL_DISPLAY_IS_VARIABLE_REFRESH_SYSTEM_PROPERTY_KEY), res);
 
     char* json_str = cJSON_Print(json);
 
@@ -573,9 +571,8 @@ int main() {
     cJSON* renderer = cJSON_GetObjectItemCaseSensitive(json, "renderer");
     cJSON* isResponsive = cJSON_GetObjectItemCaseSensitive(json, "isResponsive");
     cJSON* isH264 = cJSON_GetObjectItemCaseSensitive(json, "isH264");
-    cJSON* isVariableRefresh = cJSON_GetObjectItemCaseSensitive(json, "isVariableRefresh");
-    if (!cJSON_IsNumber(width) || !cJSON_IsNumber(height) || !cJSON_IsNumber(density) || !cJSON_IsNumber(lowres) || !cJSON_IsNumber(renderer) || !cJSON_IsNumber(isResponsive) || !cJSON_IsNumber(isH264) || 
-!cJSON_IsNumber(isVariableRefresh) ) {
+
+    if (!cJSON_IsNumber(width) || !cJSON_IsNumber(height) || !cJSON_IsNumber(density) || !cJSON_IsNumber(lowres) || !cJSON_IsNumber(renderer) || !cJSON_IsNumber(isResponsive) || !cJSON_IsNumber(isH264)) {
         handle_error(res);
         cJSON_Delete(json);
         return;
@@ -588,10 +585,9 @@ int main() {
     int rendererSetPropertyResult = property_set(VIRTUAL_DISPLAY_RENDERER_SYSTEM_PROPERTY_KEY, std::to_string(renderer->valueint).c_str());
     int isResponsiveSetPropertyResult = property_set(VIRTUAL_DISPLAY_IS_RESPONSIVE_SYSTEM_PROPERTY_KEY, std::to_string(isResponsive->valueint).c_str());
     int isH264SetPropertyResult = property_set(VIRTUAL_DISPLAY_IS_H264_SYSTEM_PROPERTY_KEY, std::to_string(isH264->valueint).c_str());
-    int isVariableRefreshSetPropertyResult = property_set(VIRTUAL_DISPLAY_IS_VARIABLE_REFRESH_SYSTEM_PROPERTY_KEY, std::to_string(isVariableRefresh->valueint).c_str());
 
     if (widthSetPropertyResult == 0 && heightSetPropertyResult == 0 && densitySetPropertyResult == 0 && lowresSetPropertyResult == 0 && rendererSetPropertyResult == 0 && isResponsiveSetPropertyResult == 0 && 
-isH264SetPropertyResult == 0 && isVariableRefreshSetPropertyResult == 0) {
+isH264SetPropertyResult == 0) {
         handle_post_success(res);
         set_virtual_display_resolution_and_density(width->valueint, height->valueint, density->valueint);
     } else {
